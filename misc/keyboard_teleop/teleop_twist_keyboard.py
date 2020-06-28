2870d9c006338be166ceb469c9669e2232846796
yvielcastillejos <yviel.castillejos@mail.utoronto.com>
2020-06-28 14:37:51 -0400

    changed topid to caffeine/cmd_vel

--- misc/keyboard_teleop/teleop_twist_keyboard.py
+++ misc/keyboard_teleop/teleop_twist_keyboard.py
@@ -70,7 +70,7 @@ speedBindings={
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.publisher = rospy.Publisher('caffeine/cmd_vel', Twist, queue_size = 1)
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
