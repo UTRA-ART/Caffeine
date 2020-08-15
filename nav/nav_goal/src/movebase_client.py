9e7bfc7d1ae0da655a11c255ccfa4b2708f18ab6
yvielcastillejos <yvielcastillejos@mail.utoronto.ca>
2020-08-15 07:19:30 -0400

    added package for sending nav goals programmatically

---
+++ nav/nav_goal/src/movebase_client.py
@@ -0,0 +1,68 @@
+import rospy
+import sys
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from tf.transformations import quaternion_from_euler
+
+def init_node(arguments):
+    rospy.init_node('movebase_client_py')
+
+    if len(arguments) < 7:
+        rospy.logerr("Error: Insufficient Arguments - Usage is 'rosrun nav_goal Movebase_client x y z roll pitch yaw' where x is distance to move in x, y is distance to move in y, z is distance to move in z, and roll, pitch, & yaw is rotation in radians.")
+        rospy.signal_shutdown("Error: Insufficient Arguments - Usage is 'rosrun nav_goal movebase_client x y z roll pitch yaw' where x is distance to move in x, y is distance to move in y, z is distance to move in z, and roll, pitch, & yaw is rotation in radians.")
+        return 'Failure'
+    else:
+        try:
+            x = float(arguments[1])
+            y = float(arguments[2])
+            z = float(arguments[3])
+            roll = float(arguments[4])
+            pitch = float(arguments[5])
+            yaw = float(arguments[6])
+            frame = arguments[7]
+
+            return (x, y, z, roll, pitch, yaw, frame)
+        except:
+            rospy.logerr("Error: Incorrect Argument Types - All 3 arguments must be floats or integers.")
+            rospy.signal_shutdown("Error: Incorrect Argument Types - All 3 arguments must be floats or integers.")
+            return 'Failure'
+
+def movebase_client(x, y, z, roll, pitch, yaw, frame):
+    # Create an action client called "move_base" with action definition file "MoveBaseAction"
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    # Waits until the action server has started up and started listening for goals.
+    client.wait_for_server()
+
+    # Creates a new goal with the MoveBaseGoal constructor
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = frame
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    # Set goal position
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.position.z = z
+
+    # Convert roll, pitch, yaw to quaternion and set pose with roll, pitch and yaw in radians
+    q = quaternion_from_euler(roll, pitch, yaw)
+    goal.target_pose.pose.orientation.x = q[0]
+    goal.target_pose.pose.orientation.y = q[1]
+    goal.target_pose.pose.orientation.z = q[2]
+    goal.target_pose.pose.orientation.w = q[3]
+
+    # Sends goal and waits until the action is completed (or aborted if it is impossible)
+    client.send_goal(goal)
+
+    rospy.loginfo(f" Navigation Goals of {x}, {y}, {z}, {roll}, {pitch}, {yaw} sent to action server!")
+
+    # Waits for the server to finish performing the action.
+    wait = client.wait_for_result()
+
+    # If the result doesn't arrive, assume the Server is not available
+    if not wait:
+        rospy.logerr("Error in retrieving action resolution! Exiting...")
+        rospy.signal_shutdown("Error in retrieving action resolution! Exiting...")
+    else:
+    # Result of executing the action
+        return client.get_goal_status_text()
+
