1fe0feb405b6e417e4ccefdac415e4dcf920366c
yvielcastillejos <yvielcastillejos@mail.utoronto.ca>
2020-08-28 18:02:10 -0400

    merged __init__ file with script

--- nav/nav_goal/src/movebase_client.py
+++ nav/nav_goal/src/movebase_client.py
@@ -67,3 +67,17 @@ def movebase_client(x, y, z, roll, pitch, yaw, frame):
     # Result of executing the action
         return client.get_goal_status_text()
 
+if __name__ == '__main__':
+    response = init_node(sys.argv)
+
+    if response != 'Failure':
+        try:
+            x, y, z, roll, pitch, yaw, frame = response
+            if frame[0:9] == "caffeine/":
+                frame = frame[9:]
+            result = movebase_client(x, y, z, roll, pitch, yaw, f"caffeine/{frame}")
+            rospy.loginfo(result)
+        except:
+            rospy.logerr("Error - Navigation failed for unknown reasons. Exiting...")
+            rospy.signal_shutdown("Error - Navigation failed for unknown reasons. Exiting...")
+~                                                                                              
