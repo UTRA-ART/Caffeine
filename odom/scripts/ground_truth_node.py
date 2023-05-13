2ee7ecbd7aa7a75b054c575a80f63b04fc3db6b7
Vicky <vhy.huang@mail.utoronto.ca>
2023-05-13 16:43:48 -0400

    Need to fix the ground_truth_script still

--- odom/scripts/ground_truth_node.py
+++ odom/scripts/ground_truth_node.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
+import numpy
 from nav_msgs.msg import Odometry
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
+
+#to run node -> python3 ground_truth_node.py
 
 class GroundTruth:
     def __init__(self):
@@ -14,24 +17,36 @@ class GroundTruth:
         self.gazebo_roll = 0
         self.gazebo_pitch = 0
         self.gazebo_yaw = 0
-        self.corrected_yaw = 0
+        
+        self.gazebo_orientation_list = []
+        
+        self.corrected_orientation_list = []
 
         self.publisher = rospy.Publisher("ground_truth_odom", Odometry, queue_size=1)
         rospy.Subscriber("ground_truth/state", Odometry, self.callback)
 
-    def callback(gazebo_position):
+    def callback(self, gazebo_position):
         self.ground_truth_msg=gazebo_position
         self.ground_truth_msg.header.stamp=rospy.Time.now()
 
         #adjust message pose parameters here
 
-        (self.gazebo_roll,self.gazebo_pitch,self.gazebo_yaw)=euler_from_quaternion(gazebo_position.pose.pose.orientation)
+        self.gazebo_orientation_list = [gazebo_position.pose.pose.orientation.x, gazebo_position.pose.pose.orientation.y, gazebo_position.pose.pose.orientation.z, gazebo_position.pose.pose.orientation.w]
+
+        (self.gazebo_roll,self.gazebo_pitch,self.gazebo_yaw)=euler_from_quaternion(self.gazebo_orientation_list)
         
-        self.corrected_yaw = gazebo_yaw - 1.5707 #subtract 90 degrees since gazebo spawns with yaw of +90
+        self.corrected_yaw = self.gazebo_yaw - 1.5707 #subtract 90 degrees since gazebo spawns with yaw of +90
+        
+        self.corrected_orientation_list = quaternion_from_euler(self.gazebo_roll,self.gazebo_pitch,self.corrected_yaw)
 
         self.ground_truth_msg.pose.pose.position.x = gazebo_position.pose.pose.position.x + 19.5
-        self.ground_truth_msg.pose.pose.orientation = quaternion_from_euler(self.gazebo_roll,self.gazebo_pitch,self.corrected_yaw)
-        self.publisher.publish(ground_truth_msg)
+        
+        self.ground_truth_msg.pose.pose.orientation.x = self.corrected_orientation_list[0]
+        self.ground_truth_msg.pose.pose.orientation.y = self.corrected_orientation_list[1]
+        self.ground_truth_msg.pose.pose.orientation.z = self.corrected_orientation_list[2]
+        self.ground_truth_msg.pose.pose.orientation.w = self.corrected_orientation_list[3]
+
+        self.publisher.publish(self.ground_truth_msg)
 
 if __name__ == '__main__':
     node = GroundTruth()
