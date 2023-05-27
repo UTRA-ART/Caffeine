20fa23a6accb81d6e32a86032e8b44ac7bd948b4
Ammar-V <ammarmust4@gmail.com>
2023-05-27 13:46:52 -0400

    Working on Lidar transform

---
+++ cv/lane_detection/src/lane_detection_inference.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import struct
+import sys 
+import time
+import json
+import os
+
+import cv2
+import numpy as np
+import onnx
+import onnxruntime as ort 
+
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+import rospkg
+import rospy
+
+from std_msgs.msg import Header
+from cv.msg import FloatArray, FloatList
+from geometry_msgs.msg import Point
+from cv_utils import camera_projection
+
+from line_fitting import fit_lanes
+from unet_lane.Inference import Inference
+
+from threshold_lane.threshold import lane_detection
+
+class CVModelInferencer:
+    def __init__(self):
+        rospy.init_node('lane_detection_model_inference')
+        
+        self.pub = rospy.Publisher('cv/lane_detections', FloatArray, queue_size=10)
+        self.pub_raw = rospy.Publisher('cv/model_output', Image, queue_size=10)
+
+        self.bridge = CvBridge()
+        self.projection = camera_projection.CameraProjection()
+        
+        rospack = rospkg.RosPack()
+        self.model_path = rospack.get_path('lane_detection') + '/models/competition_model_4c_128.pt'
+
+        # Get the parameter to decide between deep learning and classical
+        self.classical_mode = rospy.get_param('~lane_detection_mode')
+        self.Inference = None
+        self.lane_detection = None
+
+        if self.classical_mode == 1:
+            self.lane_detection = lane_detection
+            rospy.loginfo("Lane Detection node initialized with CLASSICAL... ")
+        else:
+            self.Inference = Inference(self.model_path, False)
+            rospy.loginfo("Lane Detection node initialized with DEEP LEARNING... ")
+
+
+
+    def run(self):
+        rospy.Subscriber("image", Image, self.process_image)
+        rospy.spin()
+
+    def process_image(self, data):
+        if data == []:
+            return
+            
+        raw = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        
+        if raw is not None:
+            # Get the image
+            input_img = raw.copy()
+
+            # Do model inference 
+            mask = None
+
+            if self.classical_mode:
+                mask = self.lane_detection(input_img)
+            else:
+                mask = self.Inference.inference(input_img)
+
+
+
+            # Publish to /cv/model_output
+            img_msg = self.bridge.cv2_to_imgmsg(mask, encoding='passthrough')
+            if img_msg is not None:
+                self.pub_raw.publish(img_msg)
+            
+
+            '''The following code is needed for virtual layers'''
+            # Publish to /cv/lane_detection
+            lanes = fit_lanes(mask)
+            if lanes is not None:
+                # rospy.loginfo("lanes: %s", lanes)
+                lanes_msgs = []
+                for lane in lanes:
+                    project_points = self.projection(np.array(lane, dtype=np.uint8))
+
+                    lane_msg = FloatList()
+                    pts_msg = []
+                    for pt in project_points:
+                        pt_msg = Point()
+                        pt_msg.x = pt[0]
+                        pt_msg.y = pt[1]
+                        pt_msg.z = pt[2]
+                        pts_msg += [pt_msg]
+                    lane_msg.elements = pts_msg
+                    lanes_msgs += [lane_msg]
+
+                msg_header = Header(frame_id='left_camera_link_optical')
+                msg = FloatArray(header=msg_header, lists=lanes_msgs)
+                self.pub.publish(msg)
+
+                
+
+
+if __name__ == '__main__':
+    wrapper = CVModelInferencer()
+    wrapper.run()
---
+++ cv/lane_detection/src/lane_viz.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import rospy
+from cv.msg import FloatArray
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import PointCloud2, PointField
+from sensor_msgs import point_cloud2
+# from pcl_msgs.msg import PointXYZ
+from std_msgs.msg import Header
+
+
+class FloatArrayToPointCloud2Node:
+    def __init__(self):
+        rospy.init_node("float_array_to_pc2_node")
+        self.points2_pub = rospy.Publisher(
+            "/cv/lane_detections_cloud", PointCloud2, queue_size=1
+        )
+
+    def run(self):
+        self.float_array_sub = rospy.Subscriber(
+            "/cv/lane_detections", FloatArray, self.float_array_callback, queue_size=10
+        )
+
+        rospy.spin()
+    def float_array_callback(self, msg):
+        points = []
+        for float_list in msg.lists:
+            for element in float_list.elements:
+                point = [element.y, element.x, element.z]
+                points.append(point)
+
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
+          PointField('y', 4, PointField.FLOAT32, 1),
+          PointField('z', 8, PointField.FLOAT32, 1),
+          ]
+
+        header = Header(frame_id='/left_camera_link_optical')
+
+        output_msg = point_cloud2.create_cloud(header, fields, points)
+        self.points2_pub.publish( output_msg)
+
+
+if __name__ == "__main__":
+    float_array_to_pc2_node = FloatArrayToPointCloud2Node()
+    float_array_to_pc2_node.run()
