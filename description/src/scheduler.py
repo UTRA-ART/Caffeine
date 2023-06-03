75bdff501fc72c14fddb2d1330e48dc4750fa45f
vhyhuang <69325418+vhyhuang@users.noreply.github.com>
2023-06-03 17:32:53 -0400

    CAFF-150: Update scheduler.py and Update Wheel Separation After Modification at IGVC (#154)
    * Modified scheduler.py to add SSH
    
    * Added comments
    
    * Corrected folder name
    
    * Corrected folder path in scheduler and made test script for ssh
    
    * SSH test will only run for raspberrypi2
    
    * Corrected syntax error
    
    * Corrected syntax error
    
    * Fixed ssh commands
    
    * No longer using raspberry pi 2
    
    * Added cartographer and reworked launch order
    
    * Fixed close_ssh function
    
    * Added method to unsubscribe from all topics
    
    * Corrected motor control launch command
    
    * Added motor_odom_node launch
    
    * Moved order of utm
    
    * Update wheel separation
    
    * Corrected topic name of cv laser scan
    
    * Added wait for /scan_merged
    
    * Merged scan timeout increased to 40 sec
    
    * Corrected merged scan bool name
    
    * Added upper lidar start wait
    
    * navsat node published to wrong topic, corrected and moved UTM up in scheduler
    
    * Turned off ekf tfs for own reference
    
    * Moved UTM to launch before ekf odom launches
    
    * Read through all of scheduler
    
    * Commented out robot_localization
    
    ---------
    
    Co-authored-by: Vicky <vhy.huang@mail.utoronto.ca>

--- description/src/scheduler.py
+++ description/src/scheduler.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import os
 import time
+import paramiko #need to install paramiko Python package
 
 import rospy
 import tf
 from std_msgs.msg import Bool
-from sensor_msgs.msg import NavSatFix, LaserScan, Imu, Image
+from sensor_msgs.msg import NavSatFix, LaserScan, Imu, Image, PointCloud2
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
 
 class Scheduler:
     '''
@@ -20,17 +22,19 @@ class Scheduler:
         self.gps_started = False
         self.zed_started = False
         self.imu_started = False
-        self.lidar_started = False
+        self.lower_lidar_started = False
+        self.upper_lidar_started = False
         self.assign_topic('/gps/fix', 'gps_started', NavSatFix)
         self.assign_topic('/zed_node/rgb/image_rect_color', 'zed_started', Image)
         self.assign_topic('/imu/data', 'imu_started', Imu)
-        self.assign_topic('/scan', 'lidar_started', LaserScan)
+        self.assign_topic('/scan', 'lower_lidar_started', LaserScan)
+        self.assign_topic('/scan_upper','upper_lidar_started', LaserScan)
 
         # Frames ???? How to do this 
         self.utm_published = False
         self.map_published = False
         # self.assign_topic('/imu/data', 'imu_started', None)
-        # self.assign_topic('/scan', 'lidar_started', None)
+        # self.assign_topic('/scan', 'lower_lidar_started', None)
 
         # Odometry 
         self.odom_global_published = False
@@ -42,12 +46,28 @@ class Scheduler:
         self.assign_topic('/odometry/local', 'odom_local_published', Odometry)
         self.assign_topic('/odom', 'odom_motor_published', Odometry)
 
+        #Cartographer
+        self.tracked_pose_set = False
+        self.assign_topic('/tracked_pose', 'tracked_pose_set', PoseStamped)
+
         # Meta
         self.manual_default_set = False
         self.scan_override_set = False 
+        #self.cv_cloud_set = False
+        self.cv_lane_scan_set = False
+        self.merged_scan_set = False
         self.assign_topic('/pause_navigation', 'manual_default_set', Bool, True)
         self.assign_topic('/scan_modified', 'scan_override_set', LaserScan)
+        #self.assign_topic('/cv/lane_detections_cloud','cv_cloud_set',PointCloud2)
+        self.assign_topic('/cv/lane_detections_scan2','cv_lane_scan_set',LaserScan)
+        self.assign_topic('/scan_merged','merged_scan_set',LaserScan)
 
+        #SSH
+        self.raspberry_pi2 = "10.0.0.2" #IP Address
+        self.raspberry_pi3 = "10.0.0.3" #IP Address
+        self.username = "ubuntu"
+        self.password = "utraart2021"
+    
     def abort(self, msg):
         rospy.logerr("SOMETHING FAILED. ABORTING. THIS CAUSED IT:" + msg)
         # time.sleep(100000)
@@ -90,6 +110,39 @@ class Scheduler:
         if time.time() - start >= timeout:
             self.abort(target_frame)
             raise RuntimeError(target_frame + 'frame not started in time.')
+
+    def initiate_ssh(self, ip_address, username, password):
+        if ip_address == self.raspberry_pi2:
+            rospy.loginfo('Initiating SSH client to Raspberry Pi 2')
+        elif ip_address == self.raspberry_pi3:
+            rospy.loginfo('Initiating SSH client to Raspberry Pi 3')
+
+        self.ssh_client = paramiko.client.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh_client.connect(ip_address, username=username, password=password)
+
+        if ip_address == self.raspberry_pi2:
+            rospy.loginfo('SSH client to Raspberry Pi 2 is active')
+        elif ip_address == self.raspberry_pi3:
+            rospy.loginfo('SSH client to Raspberry Pi 3 is active')
+
+    def close_ssh(self):
+        ip_address, port = self.ssh_client.get_transport().getpeername()
+        if ip_address == self.raspberry_pi2:
+            rospy.loginfo('Closing SSH client to Raspberry Pi 2')
+        elif ip_address == self.raspberry_pi3:
+            rospy.loginfo('Closing SSH client to Raspberry Pi 3')
+
+        self.ssh_client.close()
+
+        if ip_address == self.raspberry_pi2:
+            rospy.loginfo('SSH client to Raspberry Pi 2 is closed')
+        elif ip_address == self.raspberry_pi3:
+            rospy.loginfo('SSH client to Raspberry Pi 3 is closed')
+
+    def unsubscribe_all(self):
+        for subscriber in self.subs:
+            subscriber.unregister()
     
     def run(self):
         listener = tf.TransformListener()
@@ -114,49 +167,80 @@ class Scheduler:
         rospy.loginfo('Starting up sensors...')
         os.system('roslaunch sensors sensors.launch launch_state:=IGVC frame_id:=gps_link &> /dev/null &')
         self.wait_for_condition('imu_started', 35)
-        self.wait_for_condition('lidar_started', 35)
+        self.wait_for_condition('lower_lidar_started', 35)
+        self.wait_for_condition('upper_lidar_started', 40)
         self.wait_for_condition('gps_started', 90)
         rospy.loginfo('Sensors launched.')
+
+        # override scan 
+        rospy.loginfo('Launching scan override...')
+        os.system('roslaunch filter_lidar_data filter_lidar_data.launch &> /dev/null &')
+        self.wait_for_condition('scan_override_set', 20)
+        rospy.loginfo('Scan overriden.')
 
         # Run cv pipeline, wait for zed 
         rospy.loginfo('Starting CV pipeline...')
         os.system('roslaunch cv pipeline.launch launch_state:=IGVC &> /dev/null &')
         self.wait_for_condition('zed_started', 35)
+        #self.wait_for_condition('cv_cloud_set', 30)
+        self.wait_for_condition('cv_lane_scan_set', 30)
+        self.wait_for_condition('merged_scan_set', 40)
         rospy.loginfo('CV pipeline launched.')
-
-        # Run motor control and feedback, wait for /odom 
+        
+        # Run motor control, teleop and feedback, wait for /odom 
         rospy.loginfo('Starting motor controls...')
-        os.system('roslaunch description motor_control_pipeline.launch launch_state:=IGVC &> /dev/null &')
+        self.initiate_ssh(self.raspberry_pi3, self.username, self.password)
+
+        _stdin, _stdout, _stderr = self.ssh_client.exec_command('cd ~/caffeine_ws && source devel/setup.bash && roslaunch motor_control teleop_motor_control.launch &> /dev/null &')
+        print(_stdout.read().decode()) #prints the stdout of the command
+
+        #os.system('roslaunch description motor_control_pipeline.launch launch_state:=IGVC &> /dev/null &')
         # self.wait_for_transform(listener, 'base_link', 'odom')
         # self.wait_for_condition('odom_motor_published', 30)
         rospy.loginfo('Motor controls started.')
+        self.close_ssh()
 
-        # Run odom, wait for odom local, global, and /utm
-        rospy.loginfo('Initializing odometry...')
-        os.system('roslaunch odom odom.launch launch_state:=IGVC &> /dev/null &')
-        self.wait_for_condition('odom_global_published', 35)
-        self.wait_for_condition('odom_local_published', 35)
-        # self.wait_for_condition('odom_gps_published', 25)
-        rospy.loginfo('Odometry initialized.')
-        
-        # override scan 
-        rospy.loginfo('Launching scan override...')
-        os.system('roslaunch filter_lidar_data filter_lidar_data.launch launch_state:=IGVC &> /dev/null &')
-        self.wait_for_condition('scan_override_set', 20)
-        rospy.loginfo('Scan overriden.')
+        #Run motor_odom_node, wait for /odom
+        rospy.loginfo('Starting motor_odom_node...')
+        os.system('roslaunch motor_odom motor_odom.launch launch_state:=IGVC &> /dev/null &')
+        self.wait_for_condition('odom_motor_published', 30)
+        rospy.loginfo('motor_odom_node launched.')
 
-        # Run nav stack --- load_waypoints out, wait for /map /scan_modified
+        # Run utm frame transform,wait for odometry/gps
+        rospy.loginfo('Initializing UTM...')
+        os.system('roslaunch description utm.launch &> /dev/null &')
+        self.wait_for_transform(listener, '/map', '/utm')
+        self.wait_for_condition('odom_gps_published', 25)
+        rospy.loginfo('UTM initialized.')
+
+        # # Run odom, wait for odom local, global
+        # rospy.loginfo('Initializing odometry...')
+        # #self.initiate_ssh(self.raspberry_pi2, self.username, self.password)
+
+        # #_stdin, _stdout, _stderr = self.ssh_client.exec_command('cd ~/caffeine_ws && source devel/setup.bash && roslaunch odom odom.launch launch_state:=IGVC &> /dev/null &')
+        # #print(_stdout.read().decode()) #prints out the stdout of the command
+
+        # os.system('roslaunch odom odom.launch launch_state:=IGVC &> /dev/null &')
+        # self.wait_for_condition('odom_global_published', 35)
+        # self.wait_for_condition('odom_local_published', 35)
+
+        # rospy.loginfo('Odometry initialized.')
+        # #self.close_ssh()
+
+        #Run cartographer
+        rospy.loginfo('Starting cartographer...')
+        os.system('roslaunch description cartographer.launch launch_state:=IGVC &> /dev/null &')
+        self.wait_for_condition('tracked_pose_set', 60)
+        self.wait_for_transform(listener, '/odom','/base_link')
+        self.wait_for_transform(listener, '/map', '/odom')
+        rospy.loginfo('Cartographer launched.')
+
+        # Run nav stack --- load_waypoints out, wait for /map
         rospy.loginfo('Initializing navigation stack...')
         os.system('roslaunch nav_stack move_base.launch launch_state:=IGVC &> /dev/null &')
         self.wait_for_transform(listener, '/base_link', '/map')
         # self.wait_for_transform(listener, '/map', '/utm')
         rospy.loginfo('Navstack initialized.')
-
-        # Run utm frame transform
-        rospy.loginfo('Initializing UTM...')
-        os.system('roslaunch description utm.launch &> /dev/null &')
-        self.wait_for_transform(listener, '/map', '/utm')
-        rospy.loginfo('UTM initialized.')
         
         # load waypoints 
         rospy.loginfo('Loading waypoints...')
@@ -164,9 +248,9 @@ class Scheduler:
         rospy.loginfo('Waypoints loaded.') 
 
         # teleop 
-        rospy.loginfo('Starting teleop...')
-        os.system('roslaunch teleop_twist_keyboard keyboard_teleop.launch')
-        rospy.loginfo('Teleop launched.')
+        #rospy.loginfo('Starting teleop...')
+        #os.system('roslaunch teleop_twist_keyboard keyboard_teleop.launch')
+        #rospy.loginfo('Teleop launched.')
 
         return True
 
@@ -175,5 +259,6 @@ if __name__=='__main__':
     scheduler = Scheduler()
     if scheduler.run():
         rospy.loginfo("Scheduler succeeded. Caffeine is ready to rumble!")
+        scheduler.unsubscribe_all()
         while not rospy.is_shutdown():
             pass
